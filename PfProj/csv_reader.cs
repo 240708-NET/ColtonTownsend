6d74317146c15a8fcbfaf7b467dd0a148365655a
ColtonXan <coltonxtownsend@gmail.com>
2024-07-12 09:23:33 -0500

    Push from VS

--- PfProj/csv_reader.cs
+++ PfProj/csv_reader.cs
@@ -4,69 +4,65 @@
  * Procedures:
  * csvReader - default constructor
  * ReadFile - populates two vectors using a .csv at filePath.
+ * getrm - self describing
+ * getmedv - self describing
  */
 using System;
 using System.IO;
 using System.Collections.Generic;
 
-class read_csv
+namespace PfProj
 {
-	private string filePath;
-	private string rm_in, medv_in;
-	private List<double> rm;
-	private List<double> medv;
-	
-	public csvReader(string filePath)
-    {
-        filePath = filePath;
-        rm = new List<double>();
-        medv = new List<double>();
-    }
-	
-	public void ReadFile()
-    {
-        StreamReader reader;
-        string line;
-		if (!filePath)
-			filePath = "Boston.csv"; // default
-        Console.WriteLine("Opening file " + this.filePath);
-        try
-        {
-            reader = new StreamReader(this.filePath);
-            if (reader == null)
-            {
-                Console.WriteLine("Could not open file " + this.filePath);
-				return; // stop further execution
-            }
-            Console.WriteLine("Reading line 1");
-            line = reader.ReadLine();
-			Console.WriteLine("Heading: " + line);
-			
-			int numObservations = 0;
-			while ((line = sr.ReadLine()) != null){
-                    var values = line.Split(',');
-                    rm_in = values[0];
-                    medv_in = values[1];
-
-                    rm.Add(Convert.ToDouble(rm_in));
-                    medv.Add(Convert.ToDouble(medv_in));
-
-                    numObservations++;
-                }
+	public class Reader
+	{
+		private string filePath;
+		private string rm_in, medv_in; // avg, median
+		private List<double> rm = new List<double>();
+		private List<double> medv = new List<double>();
+		private int numObservations = 0;
+		
+		public Reader(){}
+		public void ReadFile(string fP, int observationLimit)
+		{
+			StreamReader reader;
+			string line;
+			filePath = fP;
+			Console.WriteLine("Opening file " + this.filePath);
+			try
+			{
+				reader = new StreamReader(this.filePath);
+				if (reader == null)
+				{
+					Console.WriteLine("Could not open file " + this.filePath);
+					return; // stop further execution
+				}
+				Console.WriteLine("Reading line 1");
+				line = reader.ReadLine();
+				Console.WriteLine("Heading: " + line);
 				
-			rm.resize(numObservations);
-			medv.resize(numObservations);
-			Console.WriteLine("New length: " + rm.Count);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Error: " + e.Message);
-            return;
-        }
-        finally
-        {
-            if (reader != null)
+				while ((line = reader.ReadLine()) != null && (numObservations < observationLimit)){
+						var values = line.Split(',');
+						rm_in = values[0];
+						medv_in = values[1];
+
+						rm.Add(Convert.ToDouble(rm_in));
+						medv.Add(Convert.ToDouble(medv_in));
+
+						numObservations++;
+					}
+					
+				//Console.WriteLine("Length: " + rm.Count);
 				Console.WriteLine("Closing file");
-                reader.Close();
-        }
-    }
+				reader.Close();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Error: " + e.Message);
+				return;
+			}
+		} //EoF
+		public List<double> getrm(){ return rm; }
+		public List<double> getmedv(){ return medv; }
+		public int getObservations(){return numObservations; }
+	}
+}
